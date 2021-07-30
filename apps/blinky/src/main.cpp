848c69e5eacce11641a962b3f8a659e4b7da3f24
Ajay Srivastava <ajaysriv3@gmail.com>
2021-07-29 23:06:21 -0700

    use iostream - add DTCM sections to csf flash in linker

--- apps/blinky/src/main.cpp
+++ apps/blinky/src/main.cpp
@@ -1,13 +1,18 @@
+#include <iostream>
+
 #include <Arduino.h>
 
 int main()
 {
-	pinMode(13, OUTPUT);
-	while (true)
-	{
-		digitalWriteFast(13, HIGH);
-		delay(1000);
-		digitalWriteFast(13, LOW);
-		delay(200);
-	}
+    pinMode(13, OUTPUT);
+
+    size_t counter = 0UL;
+    while (true)
+    {
+        digitalWriteFast(13, HIGH);
+        delay(1000);
+        digitalWriteFast(13, LOW);
+        delay(200);
+        std::cout << "Hello world" << ++counter << '\n';
+    }
 }
---
+++ common/core/write.cpp
@@ -0,0 +1,4 @@
+extern "C" int _write(int fd [[gnu::unused]], char *ptr, int len)
+{
+    return Serial.write(ptr, len);
+}
